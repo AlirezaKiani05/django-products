95947c3faf2c91ff6a9ed2b15e0b12759afb1484
elham <akiani083@gmail.com>
2024-09-17 11:54:55 +0330

    Products model added.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey(
+        'self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(
+        _('avatar'), blank=True, upload_to='categories/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('update'), auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+
+class Product(models.Model):
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='products/')
+    is_enable = models.BooleanField(default=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('update'), auto_now=True)
+    categories = models.ManyToManyField(
+        'Category', verbose_name=_('categories'), blank=True)
+
+
+class File(models.Model):
+    products = models.ManyToManyField(
+        'Product', verbose_name=_('products'), blank=True)
+    title = models.CharField(max_length=20)
+    file = models.FileField('file', upload_to='files/%Y/%m/%d')
+    is_enable = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
